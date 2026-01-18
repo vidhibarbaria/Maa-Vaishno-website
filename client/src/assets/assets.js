4960ed58196c5f10a4b6fe168e4e662a2973c589
vidhibarbaria <vidhibarbaria00@gmail.com>
2026-01-18 22:55:45 +0530

    Fix gitignore

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,5 +1,4 @@
 import logo from './logo.svg'
-import marvelLogo from './marvelLogo.svg'
 import googlePlay from './googlePlay.svg'
 import appStore from './appStore.svg'
 import screenImage from './screenImage.svg'
@@ -7,7 +6,6 @@ import profile from './profile.png'
 
 export const assets = {
     logo,
-    marvelLogo,
     googlePlay,
     appStore,
     screenImage,
